f0dfe8536121c051f939027773a9f5ff77439396
lalitoct <lalit.nagar@octalsoftware.com>
2024-06-03 12:47:26 +0530

    Update before_install.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -4,9 +4,8 @@
 cd /app
 
 # install node and npm
-apt-get install curl
-curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
-apt-get install nodejs -y
-apt-get install npm -y
-apt install nginx -y
-ufw allow 'Nginx HTTP'
+sudo apt-get install curl
+sudo curl -fsSL https://deb.nodesource.com/setup_20.x | sudo -E bash -
+sudo apt-get install nodejs -y
+sudo apt-get install npm -y
+sudo apt install apache2 -y
